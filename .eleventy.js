8d8132d04a645513da51e48334563a79b670f922
Alisher Makhmudov <makhmudov.alisher@gmail.com>
2023-10-13 12:12:57 +0500

    feat: create responsive views

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,7 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addPassthroughCopy("./src/img/");
-  eleventyConfig.addPassthroughCopy("./src/fonts/");
-  eleventyConfig.addPassthroughCopy("./src/js/");
-  eleventyConfig.addPassthroughCopy("./src/videos/");
+  eleventyConfig.addPassthroughCopy("./src/assets/");
 
-  eleventyConfig.addWatchTarget("./src/css/");
-  eleventyConfig.addWatchTarget("./src/img/");
-  eleventyConfig.addWatchTarget("./src/fonts/");
-  eleventyConfig.addWatchTarget("./src/js/");
-  eleventyConfig.addWatchTarget("./src/videos/");
+  eleventyConfig.addWatchTarget("./src/assets/");
 
   return {
     dir: {
