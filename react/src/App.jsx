58c6fb3705eef03c09aae375f224a4a2c261614f
amakhmudov <makhmudov.alisher@gmail.com>
2025-12-04 18:44:42 +0500

    start to create react version

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main className="main">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam nulla suscipit autem repellendus? Iusto non culpa est. Molestias repudiandae quis, sit cupiditate ut corrupti deleniti consequatur nostrum illum non maiores.</main>
+
+      <Footer />
+    </>
+  );
+}
